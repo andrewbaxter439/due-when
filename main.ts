a7ab3fb528fab1e49e0b78083c7996aa1fdc231b
Andrew Baxter <andrewbaxter439@gmail.com>
2023-04-26 13:18:50 +0100

    added and tidied never tag

--- main.ts
+++ main.ts
@@ -5,11 +5,15 @@ import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Set
 interface DueWhenSettings {
 	thisWeekTag: string;
 	nextWeekTag: string;
+	thisMonthTag: string;
+	neverTag: string;
 }
 
 const DEFAULT_SETTINGS: DueWhenSettings = {
 	thisWeekTag: "#thisweek",
-	nextWeekTag: "#ongoing",
+	nextWeekTag: "#upcoming",
+	thisMonthTag: "#ongoing",
+	neverTag: "#abandoned",
 }
 
 export default class DueWhen extends Plugin {
@@ -18,45 +22,16 @@ export default class DueWhen extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// My insert date command
+		// Insert due date end of week
 		this.addCommand({
 			id: 'end-of-this-week',
 			name: 'Due end of this week',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
 				let date = new Date();
-
+				
 				const dow = date.getDay();
 				const dom = date.getDate();
-
+				
 				let add_days = (12 - dow) % 7;
 				
 				date.setDate(dom + add_days);
@@ -64,7 +39,8 @@ export default class DueWhen extends Plugin {
 				editor.replaceSelection("[due:: " + date.toISOString().split('T')[0] + "] " + this.settings.thisWeekTag);
 			}
 		});
-		// My insert date next week command
+		
+		// Insert due date end of next week
 		this.addCommand({
 			id: 'end-of-next-week',
 			name: 'Due end of next week',
@@ -77,20 +53,52 @@ export default class DueWhen extends Plugin {
 				let add_days = (12 - dow) % 7;
 				
 				date.setDate(dom + add_days + 7);
-
+				
 				editor.replaceSelection("[due:: " + date.toISOString().split('T')[0] + "] " + this.settings.nextWeekTag);
 			}
 		});
+		
+		// Insert due date end of this month
+		this.addCommand({
+			id: 'end-of-this-month',
+			name: 'Due end of this month',
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				let date = new Date();
+				
+				const month = date.getMonth();
+				
+				date.setMonth(month + 1);
+				date.setDate(0);
+				
+				editor.replaceSelection("[due:: " + date.toISOString().split('T')[0] + "] " + this.settings.thisMonthTag);
+			}
+		});
+		
+		// Insert due never
+		this.addCommand({
+			id: 'never-gonna-happen',
+			name: 'Due after I\'m gone',
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				let date = new Date();
+				
+				const year = date.getFullYear();
+				
+				date.setFullYear(year + 100);
+				
+				editor.replaceSelection("[due:: " + date.toISOString().split('T')[0] + "] " + this.settings.neverTag);
+			}
+		});
+		
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new DueWhenSettingsTab(this.app, this));
-
+		
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
 		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
 			console.log('click', evt);
 		});
-
+		
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
@@ -155,11 +163,35 @@ class DueWhenSettingsTab extends PluginSettingTab {
 			.setName('Next week tag')
 			.setDesc('Tag to add to due dates set at the end of next week')
 			.addText(text => text
-				.setPlaceholder('#ongoing')
+				.setPlaceholder('#upcoming')
 				.setValue(this.plugin.settings.nextWeekTag)
 				.onChange(async (value) => {
 					console.log('New tag for next week: ' + value);
 					this.plugin.settings.nextWeekTag = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('This month tag')
+			.setDesc('Tag to add to due dates set at the end of this month')
+			.addText(text => text
+				.setPlaceholder('#ongoing')
+				.setValue(this.plugin.settings.thisMonthTag)
+				.onChange(async (value) => {
+					console.log('New tag for this month: ' + value);
+					this.plugin.settings.thisMonthTag = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Never tag')
+			.setDesc('Tag to add to due dates set after you\'re gone and it\'s no longer your problem')
+			.addText(text => text
+				.setPlaceholder('#abandoned')
+				.setValue(this.plugin.settings.neverTag)
+				.onChange(async (value) => {
+					console.log('New tag for this month: ' + value);
+					this.plugin.settings.neverTag = value;
 					await this.plugin.saveSettings();
 				}));
 	}
