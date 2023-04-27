2f17d636730d60e935202116c3b3ea969ce52876
Andrew Baxter <andrewbaxter439@gmail.com>
2023-04-27 12:16:30 +0100

    Tidy strings

--- main.ts
+++ main.ts
@@ -38,7 +38,7 @@ export default class DueWhen extends Plugin {
 				
 				date.setDate(dom + add_days);
 				
-				editor.replaceSelection("[due:: " + date.toISOString().split('T')[0] + "] " + this.settings.thisWeekTag);
+				editor.replaceSelection(` [due:: ${date.toISOString().split('T')[0]}] ${this.settings.thisWeekTag} `);
 			}
 		});
 		
@@ -56,7 +56,7 @@ export default class DueWhen extends Plugin {
 				
 				date.setDate(dom + add_days + 7);
 				
-				editor.replaceSelection("[due:: " + date.toISOString().split('T')[0] + "] " + this.settings.nextWeekTag);
+				editor.replaceSelection(` [due:: ${date.toISOString().split('T')[0]}] ${this.settings.nextWeekTag} `);
 			}
 		});
 		
@@ -82,8 +82,7 @@ export default class DueWhen extends Plugin {
 						date.setDate(date.getDate() - 1)
 					}
 				}
-				
-				editor.replaceSelection("[due:: " + date.toISOString().split('T')[0] + "] " + this.settings.thisMonthTag);
+				editor.replaceSelection(` [due:: ${date.toISOString().split('T')[0]}] ${this.settings.thisMonthTag} `);
 			}
 		});
 		
@@ -98,7 +97,7 @@ export default class DueWhen extends Plugin {
 				
 				date.setFullYear(year + 100);
 				
-				editor.replaceSelection("[due:: " + date.toISOString().split('T')[0] + "] " + this.settings.neverTag);
+				editor.replaceSelection(` [due:: ${date.toISOString().split('T')[0]}] ${this.settings.neverTag} `);
 			}
 		});
 		
@@ -106,14 +105,6 @@ export default class DueWhen extends Plugin {
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new DueWhenSettingsTab(this.app, this));
 		
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-		
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
